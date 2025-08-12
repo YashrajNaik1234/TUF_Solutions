e65844577530c2b643580dbc99a5f3527fc7e1e8
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-08-12 15:28:30 +0530

    Create Preorder Traversal.cpp

---
+++ Preorder Traversal.cpp
@@ -0,0 +1,19 @@
+class Solution{
+	public:
+		vector<int> preorder(TreeNode* root){
+	       //your code goes here
+            vector<int> ans;
+            if(root == nullptr) return ans;
+
+            stack<TreeNode*> st; st.push(root);
+            while(!st.empty()){
+                TreeNode* node = st.top(); st.pop();
+                ans.push_back(node -> data);
+                
+                if(node -> right) st.push(node -> right);
+                if(node -> left) st.push(node -> left);
+            }
+
+            return ans;
+		}
+};
