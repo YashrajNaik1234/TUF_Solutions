87c0735134617ea984960add11d8b99941f196aa
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-08-13 12:56:38 +0530

    Create Bottom view of BT.cpp

---
+++ Bottom view of BT.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    vector <int> bottomView(TreeNode *root){
+    	//your code goes here
+        vector<int> ans;
+        if(!root) return ans;
+
+        map<int,int> mp;
+        queue<pair<TreeNode*,int>> q;
+        q.push({root, 0});
+
+        while(!q.empty()){
+            auto x = q.front(); q.pop();
+
+            TreeNode* node = x.first;
+            int v = x.second;
+
+            if(mp.find(v) == mp.end()) mp[v] = node -> data;
+            else mp[v] = node -> data;
+
+            if(node -> left) q.push({node -> left, v - 1});
+            if(node -> right) q.push({node -> right, v + 1});
+        }
+
+        for(auto i: mp){
+            ans.push_back(i.second);
+        }
+
+        return ans;
+    }
+};
