e56849afc8aaa2d3f1ae776b8588c72abd48f88f
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-08-15 20:31:02 +0530

    Create Morris Inorder Traversal.cpp

---
+++ Morris Inorder Traversal.cpp
@@ -0,0 +1,33 @@
+class Solution {
+   public:
+    vector<int> getInorder(TreeNode* root) {
+        // your code goes here
+       vector<int> ans;
+
+        TreeNode* cur = root;
+        while (cur != nullptr) {
+            if (cur -> left == nullptr) {
+                ans.push_back(cur -> data);
+                cur = cur -> right;
+            } 
+            else {
+                TreeNode* prev = cur -> left;
+                while (prev -> right != nullptr && prev -> right != cur) {
+                    prev = prev -> right;
+                }
+
+                if (prev -> right == nullptr) {
+                    prev -> right = cur;
+                    cur = cur -> left;
+                } 
+                else {
+                    prev -> right = nullptr;
+                    ans.push_back(cur -> data);
+                    cur = cur -> right;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
